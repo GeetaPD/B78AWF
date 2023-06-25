4bf593a10934255acbd7ad777d6661ab3b64229e
GeetaPD <137470017+GeetaPD@users.noreply.github.com>
2023-06-25 20:35:36 +0530

    Update test3.java
    Update in local

--- src/test/java/script/test3.java
+++ src/test/java/script/test3.java
@@ -1,6 +1,13 @@
 package script;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class test3
   {
-   
+   @Test
+    public void test()
+    {
+      Reporter.log("Hi",true);
+    }
   }
